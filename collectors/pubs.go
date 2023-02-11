30287feb95cd2768e7b474792469e5a7b8f47f93
Caleb Rivera <calebrossr@gmail.com>
2023-02-10 20:27:59 -0500

    Abstract publications collector to its own file

---
+++ collectors/pubs.go
@@ -0,0 +1,121 @@
+package collectors
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+func GetPublications() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.eecs.ucf.edu"),
+	)
+
+	publications := []Publication{}
+
+	c.OnHTML("div#publications>div.row:not(.year)", func(e *colly.HTMLElement) {
+		selection := e.DOM
+
+		publicationType := strings.TrimSpace(selection.Find("div.slot-6").Text())
+		ul := selection.Find("div.slot-7-8-9>ul").Children()
+		for i := 0; i < ul.Length(); i++ {
+			li := ul.Eq(i)
+			publicationText := TrimExcessiveSpaces(li.Text())
+
+			var publication Publication
+
+			var publicationTitle string
+			var publicationURL string
+
+			authorsString := strings.Split(publicationText, "\"")[0]
+			publicationAuthors := strings.Split(authorsString, "., ")
+			publicationAuthors = cleanAuthors(publicationAuthors)
+
+			publicationYear := strings.Split(publicationText, " ")[len(strings.Split(publicationText, " "))-1]
+			publicationMonth := strings.Split(publicationText, " ")[len(strings.Split(publicationText, " "))-2]
+			publicationDate := publicationMonth + " " + publicationYear
+
+			if len(strings.Split(publicationText, "\"")) > 2 {
+				publicationTitle = strings.Split(publicationText, "\"")[1]
+			}
+
+			value, exists := li.Find("a").Attr("href")
+			if exists {
+				publicationURL = value
+			}
+
+			publication = Publication{
+				Type:    publicationType,
+				Url:     publicationURL,
+				Authors: publicationAuthors,
+				Title:   publicationTitle,
+				Date:    publicationDate,
+			}
+
+			publications = append(publications, publication)
+		}
+	})
+
+	// Create JSON file of publications
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+
+		// Encode to JSON
+		p, err := json.Marshal(publications)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if _, err := os.Stat("data"); os.IsNotExist(err) {
+			os.Mkdir("data", 0755)
+		}
+
+		if err := os.WriteFile("data/publications.json", p, 0644); err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
+
+	c.Visit("https://www.eecs.ucf.edu/isuelab/publications/")
+}
+
+func TrimExcessiveSpaces(s string) string {
+	return strings.Join(strings.Fields(s), " ")
+}
+
+func cleanAuthors(authors []string) []string {
+	newAuthors := []string{}
+
+	for _, author := range authors {
+		if len(author) == 0 {
+			continue
+		}
+
+		author = strings.Trim(author, " ")
+		author = strings.ReplaceAll(author, "and ", "")
+		author = strings.ReplaceAll(author, ".", "")
+
+		newAuthors = append(newAuthors, author)
+	}
+	return newAuthors
+}
+
+type Publication struct {
+	Title   string   `json:"title"`
+	Url     string   `json:"url"`
+	Authors []string `json:"authors"`
+	Type    string   `json:"type"`
+	Date    string   `json:"date"`
+}
--- main.go
+++ main.go
@@ -1,100 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
-	"strings"
 
-	"github.com/gocolly/colly"
+	"github.com/LightBounded/lab-site-scraper/collectors"
 )
 
-// Trims excessive spaces from a string
-func standardizeSpaces(s string) string {
-	return strings.Join(strings.Fields(s), " ")
-}
-
-type Publication struct {
-	Title   string   `json:"title"`
-	Url     string   `json:"url"`
-	Authors []string `json:"authors"`
-	Type    string   `json:"type"`
-	Date    string   `json:"date"`
-}
-
 func main() {
-	publications := []Publication{}
+	fmt.Print("What would you like to scrape? (1) Publications (2) People (3) All: ")
 
-	c := colly.NewCollector(
-		colly.AllowedDomains("www.eecs.ucf.edu"),
-	)
+	var choice int
+	fmt.Scanln(&choice)
 
-	c.OnHTML("div#publications>div.row:not(.year)", func(e *colly.HTMLElement) {
-		selection := e.DOM
-
-		publicationType := strings.TrimSpace(selection.Find("div.slot-6").Text())
-		ul := selection.Find("div.slot-7-8-9>ul").Children()
-		for i := 0; i < ul.Length(); i++ {
-			li := ul.Eq(i)
-			publicationText := standardizeSpaces(li.Text())
-
-			var publication Publication
-
-			var publicationTitle string
-			var publicationURL string
-
-			authorsString := strings.Split(publicationText, "\"")[0]
-			publicationAuthors := strings.Split(authorsString, "., ")
-
-			publicationYear := strings.Split(publicationText, " ")[len(strings.Split(publicationText, " "))-1]
-			publicationMonth := strings.Split(publicationText, " ")[len(strings.Split(publicationText, " "))-2]
-			publicationDate := publicationMonth + " " + publicationYear
-
-			if len(strings.Split(publicationText, "\"")) > 2 {
-				publicationTitle = strings.Split(publicationText, "\"")[1]
-			}
-
-			value, exists := li.Find("a").Attr("href")
-			if exists {
-				publicationURL = value
-			}
-
-			publication = Publication{
-				Type:    publicationType,
-				Url:     publicationURL,
-				Authors: publicationAuthors,
-				Title:   publicationTitle,
-				Date:    publicationDate,
-			}
-
-			publications = append(publications, publication)
-		}
-	})
-
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL)
-
-		p, err := json.Marshal(publications)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		err = os.WriteFile("publications.json", p, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
-	c.OnError(func(_ *colly.Response, err error) {
-		fmt.Println("Something went wrong:", err)
-	})
-
-	c.Visit("https://www.eecs.ucf.edu/isuelab/publications/")
-
+	switch choice {
+	case 1:
+		collectors.GetPublications()
+	case 2:
+	case 3:	
+	}
 }
